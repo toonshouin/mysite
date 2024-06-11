5b1cf52d9dd3ef3b275abb4445346546ade07898
Cartoon Kritthapath Yaviraj <krittapath6699@gmail.com>
2024-06-12 00:26:06 +0700

    First commit in 15DaysinPublic!!

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { fade } from 'svelte/transition';
+</script>
+
+<style>
+    .container {
+        text-align: center;
+        padding: 2rem;
+    }
+
+    .link {
+        cursor: pointer;
+        color: blue;
+        text-decoration: underline;
+    }
+</style>
+
+<div class="container" transition:fade>
+    <h1>Welcome to My  and yep, new site!</h1>
+    <p>This is the home page.</p>
+    <p class="link"><a href="/about">Go to About Page</a></p>
+    <p>Some temporary string here!</p>
+</div>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { fade } from 'svelte/transition';
+</script>
+
+<style>
+    .container {
+        text-align: center;
+        padding: 2rem;
+    }
+
+    .link {
+        cursor: pointer;
+        color: blue;
+        text-decoration: underline;
+    }
+</style>
+
+<div class="container" transition:fade>
+    <h1>About This Website</h1>
+    <p>This is the about page.</p>
+    <p class="link"><a href="./">Go to Home Page</a></p>
+</div>
