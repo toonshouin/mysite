672b45443e6baeca5ba04d3e83708f963e0008d2
Cartoon Kritthapath Yaviraj <krittapath6699@gmail.com>
2024-06-18 01:08:21 +0700

    Layout fix + Github Update Feature

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,57 @@
 <script>
     import { fade } from 'svelte/transition';
+    import { onMount } from 'svelte';
 
     let isVisible = false;
+
+    /**
+	 * @param {{ key: string; preventDefault: () => void; }} event
+	 */
+   function handleKeydown(event) {
+    if (event.key === 'Enter' || event.key === ' ') {
+      toggleHiddenRow();
+      event.preventDefault();  // Prevent default scrolling behavior for space key
+    }
+  }
+
+  let commitMessage = 'Loading...';
+  let commitDate = '';
+  let commitURI = 'https://github.com/toonnongaeoy/mysite'
+
+  const branch = 'v2-dev';  // specify the branch you want to fetch commits from
+  const owner = 'toonnongaeoy';
+  const repo = 'mysite';
+
+    // Function to format date to DD/MM/YYYY
+    /**
+	 * @param {string | number | Date} dateString
+	 */
+    function formatDate(dateString) {
+    const date = new Date(dateString);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // months are 0-indexed
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
+
+// Fetch the latest commit message and date on component mount
+onMount(async () => {
+    try {
+      const response = await fetch(`https://api.github.com/repos/${owner}/${repo}/commits?sha=${branch}`);
+      if (response.ok) {
+        const data = await response.json();
+        commitMessage = data[0].commit.message;
+        commitDate = formatDate(data[0].commit.author.date);
+        commitURI = data[0].html_url;
+      } else {
+        commitMessage = 'Error fetching commit data';
+        commitDate = '';
+      }
+    } catch (error) {
+      commitMessage = 'Error fetching commit data';
+      commitDate = '';
+    }
+  });
 
 function toggleHiddenRow() {
   isVisible = !isVisible;
@@ -21,7 +71,7 @@ function toggleHiddenRow() {
 
     .divider {
     background-image : linear-gradient(to right, rgba(255,0,0,0), 
-	rgba(227, 76, 45, 1), rgba(255,0,0,0));
+	rgba(223, 152, 156, 1), rgba(255,0,0,0));
     }
 </style>  
 <div class="flex justify-center" transition:fade>
@@ -29,7 +79,7 @@ function toggleHiddenRow() {
     <div class="inline">
       <span class="text-3xl font-bold">Hi! 👋</span>
       <span class="text-xl">ToonStoryTime (ToonNongAeoy) have been reconstruct this site recently! And finally ready to operate again!</span>
-      <span on:click={toggleHiddenRow} class="cursor-pointer">(Click here to read about the changes.)</span>
+      <span role="button" on:click={toggleHiddenRow} on:keydown={handleKeydown} class="cursor-pointer" tabindex="0">(Click here to read about the changes.)</span>
     </div>
     <div id="st" class={`mt-2 mb-2 ${isVisible ? 'block' : 'hidden'}`}>
       <p>
@@ -50,7 +100,36 @@ function toggleHiddenRow() {
         </p>
     </div>
     <div class={`m-3 h-0.5 divider`}> </div>
-      <p class="link"><a href="/about">Go to About Page</a></p>
-      <p>Some temporary string here!</p>
+    <div class="flex flex-row justify-center">
+      <div class="flex flex-col justify-center items-center text-center mr-4">
+        <p>current feeling :</p>
+          <img src="https://moods.imood.com/display/uname-toonnongaeoy/fg-FFFFFF/trans-1/imood.gif" width="72" style="padding:3px; padding-bottom:5px; margin-bottom:1px; background-color: #e34c2d; border:3px double #FFF2C9;">
+      </div>
+      <div class="flex flex-col mr-4 mt-0.5">
+        <div id="webring-wrapper">
+          <a href="https://webring.hackclub.com/" id="previousBtn" class="webring-anchor" title="Previous">‹</a>
+          <a href="https://webring.hackclub.com/" class="webring-logo" title="Hack Club Webring" alt="Hack Club Webring"></a>
+          <a href="https://webring.hackclub.com/" id="nextBtn" class="webring-anchor" title="Next">›</a>
+          <script src="https://webring.hackclub.com/embed.min.js"></script>
+        </div>                
+      </div>
+      <div class="flex flex-col">
+        <a href="https://webring.wonderful.software#YOUR.DOMAIN" title="วงแหวนเว็บ">
+          <img
+            alt="วงแหวนเว็บ"
+            width="36"
+            height="36"
+            src="https://webring.wonderful.software/webring.black.svg"
+            style="margin-top: 3px;"
+          />
+        </a>
+      </div>
+    </div>
+      
+    <div class={`m-3 h-0.5 divider`}> </div>
+    <div class="inline">
+      <span class="font-bold">Last Update ({commitDate}) : </span>
+      <span><a href={commitURI}>{commitMessage}</a></span>
+    </div>
   </div>
 </div>
