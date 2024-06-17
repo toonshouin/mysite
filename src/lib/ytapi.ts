ebae8774cb7d927f761ccd46e0dec227329cc742
Cartoon Kritthapath Yaviraj <krittapath6699@gmail.com>
2024-06-17 18:06:54 +0700

    Fix YT APi!

--- src/lib/ytapi.ts
+++ src/lib/ytapi.ts
@@ -1,10 +1,15 @@
 // src/lib/youtubeApi.ts
 async function fetchSubscriberCount(channelId: string): Promise<number> {
-  const apiKey = import.meta.env.YTAPI;
+  const apiKey = import.meta.env.VITE_YtA;
   const url = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelId}&key=${apiKey}`;
   const response = await fetch(url);
   const data = await response.json();
   return data.items[0].statistics.subscriberCount;
 }
 
-export { fetchSubscriberCount };
+async function getAPI() {
+  const apK = import.meta.env.VITE_YtA;
+  return apK
+}
+
+export { fetchSubscriberCount , getAPI };
