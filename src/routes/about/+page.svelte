902c42f393a2b427a44475e1badd87b59a60309d
Cartoon Kritthapath Yaviraj <krittapath6699@gmail.com>
2024-07-22 22:36:34 +0700

    Alnost FInishing About Page.

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -73,5 +73,58 @@ onMount(async () => {
         <span>For more detail of my education, you can visit my resume,</span>
         <a href="https://resume.toonstorytime.me"><span class="font-bold">Here!</span></a>
     </div>
+    <div class={`m-3 h-0.5 divider`}> </div>
+        <div class="flex flex-row">
+            <div class="flex flex-col max-w-sm w-full pr-2.5                                                                ">
+                <span class="text-3xl text-bold">Projects</span>
+                <span>- <a href="#here">Main site (toonstorytime.me)</a> My personal site</span>
+                <span>- <a href="http://toonnongaeoy.dino.icu">Other Site (toonnongaeoy.dino.icu)</a> My retro site based on <a href="https://koinuko.pink" style="color: black;">koinuko.pink</a> v.5 and 6</span>
+                <span>- <a href="https://blog.toonstorytime.me">Blog Site</a> My blog site</span>
+                <span>- <a href="https://tree.toonstorytime.me">Tree's Site</a> My linktree.</span>
+                <span>- <a href="https://wtlyrics.toonstorytime.me">What the Lyrics?</a> My lyrics site with all the weird , dirty , and everything that possible in the Lyrics!</span>
+                <span>- <a href="https://github.com/toonnongaeoy/rosepine-discord-newthemes">Rosé Pine for Discord (But in diffrent fonts)</a> Yeah, like the name suggest, and also add Rosé Pine Moon (that I like.)</span>
+                <span>- <a href="https://thai.hackclub.com">Hack Club Thai Translation Site.</a> Yeah, contribute on <a href="https://github.com/hackclub/global" style="color: black;">hackclub/global</a></span>
+            </div>
+            <div class="flex flex-col max-w-sm w-full">
+                <span class="text-3xl text-bold">Tools</span>
+                <span>- Visual Studio Code</span>
+                <span>- Git</span>
+                <span>- GitHub</span>
+                <span>- Figma</span>
+                <span>- Adobe Photoshop</span>
+                <span>- Adobe Illustrator</span>
+                <span>- Adobe Premiere Pro</span>
+                <span>- Davinci Reslove</span>
+                <span>- OBS Studio</span>
+                <span>- PyCharm</span>
+                <span>- IntelliJ</span>
+                <span>- Postman</span>
+            </div>
+        </div>
+        <br>
+        <div class="flex flex-row">
+            <div class="flex flex-col max-w-sm w-full">
+                    <span class="text-3xl text-bold">Language</span>
+                    <span>- HTML</span>
+                    <span>- CSS</span>
+                    <span>- PHP</span>
+                    <span>- Visual Basic</span>
+                    <span>- Svelte</span>       
+                    <span>- Python</span>
+                    <span>- C</span>
+                    <span>- C++</span>
+            </div>
+            <div class="flex flex-col max-w-sm w-full">
+                <span class="text-3xl text-bold">Hobby</span>
+                <span>- Web Developing</span>
+                <span>- Gaming</span>
+                <span>- Watching Anime</span>
+                <span>- Listening to Music</span>
+                <span>- Watching YouTube</span>
+                <span>- Making Youtube Videos</span>
+                <span>- Livestreaming on YouTube and Twitch</span>
+            </div>
+        </div>
+    <a href="./"><p class="text-center">Back on homepage.</p></a>
 </div>
 </div>
