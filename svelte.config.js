2a312712bec6d50dde5c29f80521eefc6c57c942
Cartoon Kritthapath Yaviraj <krittapath6699@gmail.com>
2025-03-12 05:27:19 +0700

    Fix svelte issues.

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { postcss } from 'tailwindcss';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
