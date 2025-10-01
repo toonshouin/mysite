6114b478bff2422091e51e07219cce762303ed2e
Cartoon Kritthapath Yaviraj <krittapath6699@gmail.com>
2025-10-02 00:10:49 +0700

    fix: title bar in these page

--- src/routes/playlist/+page.svelte
+++ src/routes/playlist/+page.svelte
@@ -1,5 +1,43 @@
 <script>
     import { fade } from 'svelte/transition';
+    import { onMount } from 'svelte';
+
+    let title = ''; // Reactive variable for the dynamic title
+    const fullTitle = "Music Playlist | Toonshouin's Site"; // The full title you want to display
+
+        /**
+    @param {number} i
+    */
+
+  function typeTitle(i) {
+    if (i < fullTitle.length) {
+      title += fullTitle[i];
+      setTimeout(() => typeTitle(i + 1), 250); // Adjust the speed of typing here
+    } else {
+      // Wait a bit before starting to delete the title
+      setTimeout(() => deleteTitle(fullTitle.length - 1), 1000); // Adjust the pause duration here
+    }
+  }
+
+    /**
+    @param {number} i
+    */
+
+  function deleteTitle(i) {
+    if (i >= 1) {
+      title = title.substring(0, i);
+      setTimeout(() => deleteTitle(i - 1), 200); // Adjust the speed of deleting here
+    } else {
+      // Wait a bit before starting to type again
+      setTimeout(() => typeTitle(1)); // Adjust the pause before typing again
+    }
+  }
+
+	onMount(async () => {
+		typeTitle(0);
+		// Return the cleanup function directly
+		return;
+	});
 </script>
 
 <style>
@@ -13,6 +51,11 @@
 		text-decoration: underline;
 	}
 </style>
+
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
+
 <div class="flex justify-center" transition:fade>
 <div class="container">
     <div class="text-center justify-center">
